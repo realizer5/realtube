488ef293d5ad961df278bb36257a4c05893833dd
realizer <ipreet35232@gmail.com>
2025-07-12 10:46:19 +0530

    added comment,like,playlist,tweet model & bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -77,7 +77,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-    await User.findOneAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true });
+    await User.findByIdAndUpdate(req.user._id, { $unset: { refreshToken: 1 } }, { new: true });
     return res.status(200).clearCookie("accessToken", options).clearCookie("refreshToken", options)
         .json(new ApiResponse(200, {}, "user logged out"));
 });
